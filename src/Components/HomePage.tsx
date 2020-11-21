92987edd50ca8b718699b5fc472acf3c96a8fe97
Anas Khafaga <khafaga1998@gmail.com>
2020-11-21 21:24:24 +0200

    Fetch movies data from Cloud Firestore `movies` collection

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,7 +1,58 @@
 import React, { Component } from 'react'
+import firebase from 'firebase';
 
-export default class HomePage extends Component {
+type hpProps = {
+
+}
+
+export default class HomePage extends Component<hpProps> {
+
+  state: {
+    movies: object[];
+    genres: object[];
+  }
+  
+  constructor(props: hpProps) {
+    super(props);
+    this.state = {
+      movies: [],
+      genres: []
+    }
+  };
+  
+  componentDidMount() { 
+    const db = firebase.firestore();
+    let movies: object[];
+    let genres: string[];
+    movies = [];
+    genres = [];
+    db.collection('movies').get()
+      .then(cursors => { 
+        cursors.forEach(cursor => {
+          movies.push(cursor.data());
+          cursor.data().genres.forEach((ele: string) => {
+
+            if (genres.indexOf(ele) > -1) {
+              return;
+            }
+
+            genres.push(ele);
+            
+          });
+        });
+        console.log(genres);
+        this.setState({
+          movies: [...movies],
+          genres: [...genres]
+        })
+      })
+      .catch(err => { 
+        alert('Oops! Something went wrong');
+      });
+  };
+  
   render() {
+    console.log(this.state);
     return (
       <div>
         Welcome to homepage
